d8390dcbbdf97656722e49b68af737759e832f51
Paul Lassa <plassa3@gmail.com>
2015-02-17 22:59:38 -0800

    All basic functions working!
    add item
    delete item
    save, load items list

--- app/src/main/java/com/ich/plassa/todoapp/TodoActivity.java
+++ app/src/main/java/com/ich/plassa/todoapp/TodoActivity.java
@@ -5,42 +5,89 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static android.widget.AdapterView.OnItemLongClickListener;
 
 
 public class TodoActivity extends ActionBarActivity {
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
+    EditText etNewItem;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+        items = new ArrayList<>();
+        etNewItem = (EditText) findViewById(R.id.etNewItem);
         lvItems = (ListView) findViewById(R.id.lvItems);
-        items = new ArrayList<String>();
-        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("First Item");
-        items.add("Second Item");
-        
+//        items.add("First Item");
+//        items.add("Second Item");
+//        items.add("Third Item");
+        setupListViewListener();
+
     }
 
-//        populateArrayItems();
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+    }
+//        android:text="@string/add_button_label"
+
 //        todoItemsAdapter = new ArrayAdapter<string>(this, R.layout.,objects)
 
-//    private void populateArrayItems() {
-//        todoItems = new ArrayList<string>();
-//        todoItems.add("Item 1");
-//        todoItems.add("Item 2");
-//        todoItems.add("Item 3");
-//    }
-    public void onAddItem(View v){
 
+    public void onAddItem(View v){
+//        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String ItemText = etNewItem.getText().toString();
+        itemsAdapter.add(ItemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<>(FileUtils.readLines(todoFile));
+
+        }  catch (IOException e) {
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+//            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+            FileUtils.writeLines(todoFile, items);
+        }  catch (IOException e) {
+//            items = new ArrayList<String>();
+                e.printStackTrace();
+        }
     }
 
     @Override
